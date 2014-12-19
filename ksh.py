f4c940fab7efc59e7763a434c4f863218f15fb5f
YeHe Cai <cyh991698424@sina.cn>
2014-12-19 18:40:19 +0800

    Done. No bugs.

--- ksh.py
+++ ksh.py
@@ -10,7 +10,6 @@ def sqdist(a,b):
     bb = power(b,2).sum(0).reshape((1, b.shape[1]))
 
     ab = dot(a.T,b)
-    #d = kron( ones((1, bb.shape[1])), aa.T) + kron( ones((aa.shape[0], 1)), bb) - 2*ab
     d = repmat( aa.T, 1, bb.shape[1] ) + repmat( bb, aa.shape[0], 1) - 2*ab
 
     return d
@@ -29,10 +28,10 @@ def OptProjectionFast(K, S, a0, cn):
     delta[0] = 0
     delta[1] = 1
 
-    beta    = zeros(cn+2)
+    beta    = zeros(cn+1)
     beta[0] = 1
 
-    for t in range(0, cn-1):
+    for t in range(0, cn):
         alpha = (delta[t]-1)/delta[t+1]
         v = a1 + alpha*(a1-a0)
         y0 = dot(K, v)
@@ -41,9 +40,9 @@ def OptProjectionFast(K, S, a0, cn):
         ty = multiply( dot(S,y1), (ones((n,1))-y1**2) )[0] 
         dgv = -dot( K.T, ty)
 
-        # seek beta[0]
+        # seek beta
         flag = 0
-        for j in range(0,50):
+        for j in range(0,51):
             b  = 2**j * beta[t]
             z  = v-dgv/b
             y0 = dot(K,z)
@@ -79,18 +78,20 @@ def repmat( data, r, c ):
 
 class KSH(object):
 
-    def __init__(self):
+    def __init__(self, data_file):
         print('Init the ksh...')
-        pkl = file( '../data/data.pkl', 'rb' )
+        pkl = file( data_file, 'rb' )
         Train, Test, self.label_index, self.sample = pickle.load(pkl)
         self.sample = array( self.sample, dtype=int) - 1
         self.label_index = array( self.label_index, dtype=int) - 1
         pkl.close()
+
         self.traingnd  = Train[0]
-        self.traingnd = array( self.traingnd, dtype=int) - 1
+        self.traingnd = array( self.traingnd, dtype=int)
         self.traindata = Train[1]
+
         self.testgnd   = Test[0]
-        self.testgnd = array( self.testgnd, dtype=int) - 1
+        self.testgnd = array( self.testgnd, dtype=int)
         self.testdata  = Test[1]
 
         # number of anchors
@@ -125,24 +126,26 @@ class KSH(object):
         flag = zeros( bit_num )
         
         for rr in range(0,bit_num):
-            print rr
+            print 'No: %d'%rr
             if rr > 0:
                 S = S- dot(y, y.T)
+
 
             LM = dot( dot(KK.T, S), KK )
             (V, U) = eigh( LM, RM, eigvals_only=False)
 
             
-            A1[:,rr] = U[:,299]
+            A1[:,rr] = U[:,(self.m-1)]
             tep = dot(dot( A1[:,rr].T, RM ), A1[:,rr])
             A1[:,rr] = sqrt(self.trn/tep)*A1[:,rr]
 
             
             get_vec, cost = OptProjectionFast( KK, S, A1[:,rr], 500)
+
             
             y = dot(KK, A1[:,rr])
-            y = ( y > 0).choose( y, 1)
-            y = ( y <=0).choose( y,-1)
+            y = (y> 0).choose( y, 1)
+            y = (y<=0).choose( y,-1)
             y = y.reshape(y.shape[0], 1)
 
             y1 = dot(KK, get_vec)
@@ -174,65 +177,71 @@ class KSH(object):
 
         # search
         sim = dot(Y.T, tY)
-        result_cal(sim, self.testgnd)
+        result_cal(sim, self.testgnd, self.traingnd)
+        purity_cal(Y)
+
+def purity_cal(y):
+    Y = y.T
+    hash_dict = {}
+    hash_num = -1
+    M = []
+    for i in range(0, Y.shape[0]):
+        if hash_dict.has_key(str(Y[i])):
+            M[hash_dict[str(Y[i])]][i]  = 1
+        else:
+            hash_num += 1
+            hash_dict[str(Y[i])] = hash_num
+            M.append([0]*Y.shape[0])
 
 
-def result_cal(sim, testgnd):
+    print hash_dict
+
+    pkl_file = open(sys.argv[3], 'wb')
+    pickle.dump( M, pkl_file )
+            
+
+
+
+def result_cal(sim, testgnd, traingnd):
     rank_num = [1, 5, 10, 20, 30, 50]
     findNum  = [0, 0,  0,  0,  0,  0]
     MAP      = [0, 0,  0,  0,  0,  0]
-    Precision      = [0, 0,  0,  0,  0,  0]
+    Precision= [0, 0,  0,  0,  0,  0]
 
     sim = sim.T 
+    r_n = -1
     for row in sim:
-        sort_d = mat(tuple(msort(row)[::-1]))
+        r_n += 1
+        #sort_d = mat(tuple(msort(row)[::-1]))
         index  = mat(tuple(row.argsort(axis=0)[::-1]))
-        
-        for i in range(0, testgnd.shape[0]):
-            tt = -1
-            for m in rank_num:
-                tt += 1
-                for j in range(0, m):
-                    if int(testgnd[i]) == int(index[0,j]):
-                        findNum[tt] += 1
-            #for i in range(0, m):
-            #    print testgnd[i], index[0, i]
-            #    if int(testgnd[i]) == int(index[0, i]):
-            #        findNum[t] += 1 
+        #print testgnd[r_n], index
+
+        for i in range(0 , 6):
+            flag = 0
+            for j in range(0, rank_num[i]):
+                if int(testgnd[r_n]) == int(traingnd[index[0, j]]):
+                    flag = 1
+                    findNum[i] += 1
+            if flag == 1:
+                Precision[i] += 1
 
     print findNum
+    print Precision
     for i in range(0, 6):
-        MAP[i] = (findNum[i]/rank_num[i]/testgnd.shape[0])
+        MAP[i] = (findNum[i]*1.0/rank_num[i]/testgnd.shape[0])
+        Precision[i] = Precision[i]*1.0/testgnd.shape[0]
 
     print MAP
+    print Precision
         
-
-                    
-
-
-
-
-        
-def test():
-    #a = mat('1,2,3;1,2,3')
-    #b = mat('2,3,4;2,3,4')
-    #print sqdist(a,b)
-    t = mat('2, 6, 1, 99, 4, 3, 6')
-    print msort(t.T).T
-    print t.argsort(axis=1) + 1
-
-def test2():
-    pkl = file( '../data/opt.pkl', 'rb' )
-    K, S, a0 = pickle.load(pkl)
-    pkl.close()
-
-    print(OptProjectionFast(K, S, a0,500))
-    
-
 
 if __name__ == '__main__':
     
-    #test2()
-
-    ksh = KSH()
-    ksh.ksh(8)
+    if len(sys.argv) < 4:
+        print len(sys.argv)
+        print "Usage: python ksh.py data_file hash_bit out_pkl(purity)"
+        sys.exit()
+    
+    ksh = KSH(sys.argv[1])
+    ksh.ksh(int(sys.argv[2]))
+    
